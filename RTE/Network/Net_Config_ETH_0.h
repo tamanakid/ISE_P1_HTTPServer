aa2686a498df5206b173b2155f3f2d927ad59291
tamanakid <nfvillanuevap@gmail.com>
2021-02-25 23:16:58 +0100

    Assignment - Provide Laboratory Networking Configuration

--- RTE/Network/Net_Config_ETH_0.h
+++ RTE/Network/Net_Config_ETH_0.h
@@ -53,19 +53,19 @@
 //   <i>It is a Broadcast IP address.
 //     <o>Address byte 1 <0-255>
 //     <i>Default: 192
-#define ETH0_IP1                192
+#define ETH0_IP1                10
 
 //     <o>Address byte 2 <0-255>
 //     <i>Default: 168
-#define ETH0_IP2                168
+#define ETH0_IP2                48
 
 //     <o>Address byte 3 <0-255>
 //     <i>Default: 0
-#define ETH0_IP3                1
+#define ETH0_IP3                11
 
 //     <o>Address byte 4 <0-255>
 //     <i>Default: 100
-#define ETH0_IP4                142
+#define ETH0_IP4                109
 //   </h>
 
 //   <h>Subnet mask
@@ -76,11 +76,11 @@
 
 //     <o>Mask byte 2 <0-255>
 //     <i>Default: 255
-#define ETH0_MASK2              255
+#define ETH0_MASK2              254
 
 //     <o>Mask byte 3 <0-255>
 //     <i>Default: 255
-#define ETH0_MASK3              255
+#define ETH0_MASK3              0
 
 //     <o>Mask byte 4 <0-255>
 //     <i>Default: 0
@@ -91,15 +91,15 @@
 //   <i>Default Gateway IP Address
 //     <o>Address byte 1 <0-255>
 //     <i>Default: 192
-#define ETH0_GW1                192
+#define ETH0_GW1                10
 
 //     <o>Address byte 2 <0-255>
 //     <i>Default: 168
-#define ETH0_GW2                168
+#define ETH0_GW2                48
 
 //     <o>Address byte 3 <0-255>
 //     <i>Default: 0
-#define ETH0_GW3                1
+#define ETH0_GW3                0
 
 //     <o>Address byte 4 <0-255>
 //     <i>Default: 254
